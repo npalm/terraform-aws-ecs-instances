02c47f01e6e2096bd83fb44ca1ea52def3e4e901
saifalibim <105201891+saifalibim@users.noreply.github.com>
2022-07-05 00:00:40 +0500

    Update variables.tf
    changes

--- variables.tf
+++ variables.tf
@@ -1,51 +1,51 @@
 variable "aws_region" {
   description = "The AWS region to be used."
-  type        = "string"
+  type        = string
 }
 
 variable "key_name" {
   description = "Name of AWS key pair"
-  type        = "string"
+  type        = string
 }
 
 variable "instance_type" {
   default     = "t2.small"
   description = "Default AWS instance type."
-  type        = "string"
+  type        = string
 }
 
 variable "asg_min" {
   description = "Minimum numbers of instances in the auto scaling group."
   default     = "1"
-  type        = "string"
+  type        = string
 }
 
 variable "asg_max" {
   description = "Maximum numbers of instances in the auto scaling group."
   default     = "2"
-  type        = "string"
+  type        = string
 }
 
 variable "asg_desired" {
   description = "Desired numbers of instances in the auto scaling group."
   default     = "1"
-  type        = "string"
+  type        = string
 }
 
 // new
 variable "vpc_id" {
   description = "ID of the VPC."
-  type        = "string"
+  type        = string
 }
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC."
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Logical name of the environment, will be used as prefix and in tags."
-  type        = "string"
+  type        = string
 }
 
 variable "subnets" {
@@ -72,12 +72,12 @@ variable "os" {
 
 variable "ecs_cluster_name" {
   description = "Name of the ECS cluster."
-  type        = "string"
+  type        = string
 }
 
 variable "user_data" {
   description = "Override the module embedded user data script."
-  type        = "string"
+  type        = string
   default     = ""
 }
 
